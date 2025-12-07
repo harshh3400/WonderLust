3c23c029dbdf38d5a830cacfd7032e3f01c79032
Harshad Shinde <plshinde98@gmail.com>
2025-12-07 13:42:09 +0530

    map Implemented

--- app.js
+++ app.js
@@ -159,6 +159,8 @@ app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   res.locals.currUser = req.user;
+  //map
+  res.locals.mapToken = process.env.GEOAPIFY_API_KEY;
   next();
 });
 
